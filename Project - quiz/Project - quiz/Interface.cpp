15412b29b1770fd0ca9c950315e73aeec9efc15c
Tymoteusz <92216406+TymoteuszZdanowicz@users.noreply.github.com>
2022-06-21 21:25:41 +0200

    Delete Project - quiz directory

--- Project - quiz/Project - quiz/Interface.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Question.h"
-
-using namespace std;
-
-void interface() {
-
-}
--- Project - quiz/Project - quiz/parseFile.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#ifndef PARSEFILE_H
-#define PARSEFILE_H
-void odczytDanych(string fileName);
-#endif // !PARSEFILE_H
